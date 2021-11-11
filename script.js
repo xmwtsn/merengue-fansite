62b0a87745323da81c8649ad54e5e01018a6e8f6
Xavier Watson <56239246+xmwtsn@users.noreply.github.com>
2021-11-11 06:08:14 -0500

    CSS & content update

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function buttonClick() {
     i++;
     document.getElementById('cake').value = i;
 }
+
+function sendMail() {
+    var link = "mailto:dearmerengue@gmail.com"
+             + "&body=" + encodeURIComponent(document.getElementById('myText').value)
+    ;
+    
+    window.location.href = link;
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "hmenu") {
+      x.className += " responsive";
+    } else {
+      x.className = "hmenu";
+    }
+  }
