3d28418c1cad8748a9d24d602f7ebff87dcb7b8a
Xavier Watson <56239246+xmwtsn@users.noreply.github.com>
2021-11-15 13:44:46 -0500

    revised comments

--- js/calculate.js
+++ js/calculate.js
@@ -1,6 +1,10 @@
-/* Simple JS calculation counting clicks */
+// Simple JS calculation counting clicks
+
+// Sets variable
 var i = 0;
 function buttonClick() {
+    // Applies increment operator
     i++;
+    // Targets input with 'cake' ID on index with value of variable
     document.getElementById('cake').value = i;
 }
